4d0379766d4e9ad18cd0f92ab30fe5d79bf7824d
Lexag <lexa.grebnets@gmail.com>
2026-03-22 15:19:32 +0100

    add program icon

--- src/main.rs
+++ src/main.rs
@@ -8,15 +8,39 @@ mod udp;
 mod widget;
 mod window;
 
+pub(crate) fn load_icon() -> egui::IconData {
+    let (icon_rgba, icon_width, icon_height) = {
+        let icon = include_bytes!("../images/icon-64.png");
+        let image = image::load_from_memory(icon)
+            .expect("Failed to open icon path")
+            .into_rgba8();
+        let (width, height) = image.dimensions();
+        let rgba = image.into_raw();
+        (rgba, width, height)
+    };
+
+    egui::IconData {
+        rgba: icon_rgba,
+        width: icon_width,
+        height: icon_height,
+    }
+}
+
 // When compiling natively:
 #[cfg(not(target_arch = "wasm32"))]
 pub fn main() -> eframe::Result {
     use udp::UdpClient;
 
+    let app_name = format!("ClicKS Monitor {}", env!("CARGO_PKG_VERSION"));
+
     let native_options = eframe::NativeOptions {
         viewport: egui::ViewportBuilder::default()
             .with_inner_size([1920.0, 1080.0])
-            .with_min_inner_size([300.0, 220.0]), //           .with_icon(
+            .with_min_inner_size([300.0, 220.0])
+            .with_title(&app_name)
+            .with_icon(load_icon())
+            .with_app_id("clicks-monitor"),
+        //           .with_icon(
         // NOTE: Adding an icon is optional
         //               eframe::icon_data::from_png_bytes(&include_bytes!("../assets/icon-256.png")[..])
         //                   .expect("Failed to load icon"),
@@ -28,7 +52,7 @@ pub fn main() -> eframe::Result {
     udp_client.start();
 
     eframe::run_native(
-        format!("ClicKS Monitor {}", env!("CARGO_PKG_VERSION")).as_str(),
+        &app_name,
         native_options,
         Box::new(|cc| Ok(Box::new(app::ClicksMonitorApp::new(cc, udp_client)))),
     )
